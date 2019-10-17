a89b97d74f9371ea44f48c7c4807cc42ecfdad29
olga <reamshi@mail.ru>
2019-10-17 22:42:15 +0400

    3.1

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^polls/index.html', views.index),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^polls/detail.html', views.detail),
+
 ]
